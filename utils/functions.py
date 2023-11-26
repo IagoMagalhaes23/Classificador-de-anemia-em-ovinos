12cd94d707437475b74d0d2c380a85cd397a6aca
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-11-26 14:04:11 -0300

    update file functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -3,8 +3,9 @@
     Data: 09/11/2023
     Descrição:
         - Implementa funções para leitura das imagens
-        - Aplicação de filtro de realce e composição do dataset para treino, teste e validação
+        - Composição do dataset para treino, teste e validação
         - Plotagem dos gráficos de treinamento
+        - Plotagem da matriz de confusão
 '''
 
 import os
@@ -16,6 +17,7 @@ import numpy as np
 import pandas as pd
 from time import time_ns
 import matplotlib.pyplot as plt
+plt.style.use('default')
 
 from keras.preprocessing import image
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, roc_curve, auc
@@ -24,7 +26,7 @@ def readFiles(caminhos):
     '''
         Função para ler todos os arquivos de imagem em uma pasta
         :param caminhos: caminho dos arquivos de imagem
-        : return: retorna uma lista com o endereço e nome da imagem e a respectiva classe
+        :return: retorna uma lista com o endereço e nome da imagem e a respectiva classe
     '''
     cont = 0
     data_list = []
@@ -36,54 +38,6 @@ def readFiles(caminhos):
             data_list.append([os.path.join(cam, file), file[-5]])
 
     return data_list
-
-def filtros(cam, size, filtro):
-    '''
-        Função para redimensionamento da imagens em 224x224 pixels, conversão para tons de cinza e aplicação do filtro Gaussiano
-        :param cam: enderenço, nome e formato da imagem a ser tratada
-        :param size: tamanho da imagem final
-        :param filtro: defini qual filtro será aplicado na imagem
-        :return: retorna uma imagem redimensionada e com aplicação de um filtro
-    '''
-    img = cv2.imread(cam)
-    img = cv2.resize(img, (size, size))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # img = img/255.0
-
-    if(filtro == 0):
-        elementoEstruturante = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
-        img = cv2.dilate( img, elementoEstruturante, iterations = 2)
-    elif(filtro == 1):
-        img = cv2.Laplacian(img, cv2.CV_64F)
-        img = np.uint8(np.absolute(img))
-    elif(filtro == 2):
-        img = cv2.GaussianBlur(img, ( 5, 5), 0)
-    elif(filtro == 3):
-        img = cv2.blur(img, ( 3, 3), 0)
-    elif(filtro == 4):
-        img = cv2.medianBlur(img, 3, 0)
-    elif(filtro == 5):
-        img = cv2.medianBlur(img, 5, 0)
-        _, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    elif(filtro == 6):
-        img = cv2.GaussianBlur(img, ( 3, 3), 0)
-        _, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    elif(filtro == 7):
-        img = cv2.medianBlur(img, 3, 0)
-        img = cv2.GaussianBlur(img, ( 5, 5), 0)
-    elif(filtro == 8):
-        img = cv2.medianBlur(img, 3, 0)
-        sobelX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
-        sobelY = cv2.Sobel(img, cv2.CV_64F, 0, 1)
-        sobelX = np.uint8(np.absolute(sobelX))
-        sobelY = np.uint8(np.absolute(sobelY))
-        img = cv2.bitwise_or(sobelX, sobelY)
-    elif(filtro == 9):
-        img = cv2.GaussianBlur(img, ( 3, 3), 0)
-        img = cv2.Canny(img, 20, 120)
-
-    img = np.reshape(img, (size, size, 1))
-    return img
 
 def compose_dataset(df, size, filtro):
     '''
@@ -111,18 +65,28 @@ def compose_dataset(df, size, filtro):
 
     return np.array(data), np.array(labels)
 
-def plot_hist(hist):
+def plot_hist(history):
     '''
         Função para plotar o gráfico de treinamento
-        :param hist: recebe o histórico de treinamento da rede
+        :param hist: recebe o histórico de treinamento da rede com os dados de acurácia e loss
     '''
-    plt.plot(hist.history["accuracy"])
-    plt.plot(hist.history["loss"])
-    plt.title("model accuracy")
-    plt.ylabel("accuracy")
-    plt.xlabel("epoch")
-    plt.legend(["train", "validation"], loc="upper left")
-    plt.show()
+    plt.figure(figsize=(12,10))
+    plt.subplot(2,2,1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.ylim(0.8,1)
+    plt.title('Model Accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train','valid'], loc='lower right')
+    plt.subplot(2,2,2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Loss Function')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train','valid'], loc='upper right')
+    plt.ylim([0,1])
 
 def metrics(y_actual, y_pred):
     '''
@@ -146,6 +110,8 @@ def metrics(y_actual, y_pred):
 def plot_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=None, normalize=True):
     '''
         Função para plotagem da matriz de confusão
+        :param :
+        :return: 
     '''
     accuracy = np.trace(cm) / float(np.sum(cm))
     misclass = 1 - accuracy
@@ -203,44 +169,3 @@ def roc_curve():
     # plt.legend(loc="lower right")
     # plt.show()
     print('Tu ainda precisa fazer essa função')
-
-def prepararImagem(imagem):
-    test_image = image.img_to_array(imagem.T)
-    test_image = np.expand_dims(test_image, axis = 0)    
-    return test_image
-
-def mostraCateg(resultado):
-    categs = ['A', 'B', 'C', 'D', 'E']
-    max_value = None
-    max_idx = None
-        
-    for idx, num in enumerate(resultado[0]):
-        if max_value is None or num > max_value:
-            max_value = num
-            max_idx = idx
-    print(max_value)
-    print(max_idx)
-    return categs[max_idx]
-
-def process_memory():
-  process = psutil.Process(os.getpid())
-  mem_info = process.memory_info()
-  return mem_info.rss
-
-def getMemory(funcao, W, arr):
-  start_time = time_ns()  #Pega o tempo inicial
-  # mem_before = process_memory() #Pega a memória inicial
-  tracemalloc.start()
-  #XXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-  results = funcao(W, arr)
-  mem_used = tracemalloc.get_traced_memory()
-  #XXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  # mem_after = process_memory() #Pega a memória final
-  tracemalloc.stop()
-
-  # mem_used = mem_after - mem_before  #Calcula a memória consumida em bytes
-  end_time = time_ns()  # Pega o tempo de término
-  elapsed_time = end_time - start_time  #Calcula o tempo consumido em nanossegundos
-
-  return mem_used, elapsed_time, results
