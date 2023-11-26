b02ee781d6152730d94d08d75648a607893001fe
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-11-26 20:05:05 -0300

    create function vgg16

---
+++ models/vgg16.py
@@ -0,0 +1,30 @@
+'''
+    Autores: Iago, Francilândio, Vanessa, Raniery, Sávio, Iális e Fischer
+    Data: 26/11/2023
+    Descrição:
+        - Implementa a rede VGGNet16
+'''
+
+from tensorflow import keras
+
+def vgg16():
+    base_model = keras.applications.VGG16(
+        weights='imagenet',
+        input_shape=(224, 224, 3),
+        include_top=False)
+
+    base_model.trainable = False
+
+    print(base_model.summary())
+
+    inputs = keras.Input(shape=(224, 224, 3))
+    # Separately from setting trainable on the model, we set training to False 
+    x = base_model(inputs, training=False)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    # A Dense classifier with a single unit (binary classification)
+    outputs = keras.layers.Dense(1)(x)
+    model = keras.Model(inputs, outputs)
+
+    model.compile(loss=keras.losses.BinaryCrossentropy(from_logits=True), metrics=[keras.metrics.BinaryAccuracy()])
+
+    return model
