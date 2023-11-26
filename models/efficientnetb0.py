71af4fb9fdeacf7aead5f72154c6a73fdc0d1eeb
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-11-26 20:08:20 -0300

    create function efficientNetB0

---
+++ models/efficientnetb0.py
@@ -0,0 +1,27 @@
+'''
+    Autores: Iago, Francilândio, Vanessa, Raniery, Sávio, Iális e Fischer
+    Data: 26/11/2023
+    Descrição:
+        - Implementa a rede EfficientNetB0
+'''
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.applications import EfficientNetB0
+
+def efficientNetB0():
+    NUM_CLASSES = 1
+    IMG_SIZE = 224
+    size = (IMG_SIZE, IMG_SIZE)
+
+    inputs = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 1))
+
+    # Using model without transfer learning
+
+    outputs = EfficientNetB0(include_top=True, weights=None, classes=NUM_CLASSES)(inputs)
+
+    model = keras.Model(inputs, outputs)
+
+    model.compile(optimizer="adam", loss=keras.losses.BinaryCrossentropy(from_logits=True), metrics=[keras.metrics.BinaryAccuracy()])
+
+    return model
