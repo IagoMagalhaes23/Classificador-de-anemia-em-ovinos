2c13fd379cde2d597a5795fe29f50e78106d7d55
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-11-30 16:04:55 -0300

    update main

--- main.py
+++ main.py
@@ -10,38 +10,35 @@ import random
 import pandas as pd
 import numpy as np
 
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from sklearn.model_selection import train_test_split
 
-from models import vgg16
+# from tensorflow.keras.utils import to_categorical
+# from tensorflow.keras.callbacks import EarlyStopping
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# from models import CarNet, efficientnetb0, inceptionV3, vgg16, vitB16
 from utils import functions as fct
 
-caminho_train = 'dataset/train/Mucosa'
-caminho_test = 'dataset/test/Mucosa'
-caminho_valid = 'dataset/valid'
+caminho = 'dataset/'
 
-data_train = fct.readFiles(caminho_train)
-data_test= fct.readFiles(caminho_test)
+data = fct.readFiles(caminho)
 
-random.shuffle(data_train)
-random.shuffle(data_test)
+random.shuffle(data)
 
-df_train = pd.DataFrame(data_train, columns=['image', 'x', 'y', 'w', 'h'])
-df_test = pd.DataFrame(data_test, columns=['image', 'x', 'y', 'w', 'h'])
+df = pd.DataFrame(data, columns=['image', 'label'])
 
-df_train = df_train.reset_index(drop = True)
-df_test = df_test.reset_index(drop = True)
+df = df.reset_index(drop = True)
+
+# X_train, X_test, y_train, y_test = train_test_split(df.drop(df.columns["label"], axis=1), df["label"], test_size=0.1, shuffle=False)
 
 print('----------------------------- SHAPES DF -----------------------------')
-print(df_train.shape)
-print(df_test.shape)
+print(df.shape)
 print('---------------------------------------------------------------------')
 
-X_train, y_train = fct.compose_dataset(df_train)
-X_test, y_test = fct.compose_dataset(df_test)
+# X_train, y_train = fct.compose_dataset(df_train)
+# X_test, y_test = fct.compose_dataset(df_test)
 
-print('----------------------------- SHAPES LABEL -----------------------------')
-print('Treino shape: {}, Labels shape: {}'.format(X_train.shape, y_train.shape))
-print('Teste shape: {}, Labels shape: {}'.format(X_test.shape, y_test.shape))
-print('------------------------------------------------------------------------')
+# print('----------------------------- SHAPES LABEL -----------------------------')
+# print('Treino shape: {}, Labels shape: {}'.format(X_train.shape, y_train.shape))
+# print('Teste shape: {}, Labels shape: {}'.format(X_test.shape, y_test.shape))
+# print('------------------------------------------------------------------------')
